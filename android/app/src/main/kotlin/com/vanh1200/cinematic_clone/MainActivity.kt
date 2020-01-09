adffbbcc76184ee720f876a13decd16625404aad
vanh1200 <anhkieuviet@vccorp.vn>
2020-01-09 14:48:18 +0700

    init project

---
+++ android/app/src/main/kotlin/com/vanh1200/cinematic_clone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vanh1200.cinematic_clone
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
